c68ced4a95ae712f11b3adfc98def09f99a8a8e4
victorlongon <victor.longon@hiberworld.com>
2023-02-11 21:06:51 +0100

    05 optional problem

---
+++ 05-optionalProblem.test.ts
@@ -0,0 +1,35 @@
+import { expect, it } from 'vitest';
+import { z } from 'zod';
+
+const Form = z.object({
+  name: z.string({ required_error: 'name is required' }),
+  phoneNumber: z.string().optional(),
+  //                     ^ 🕵️‍♂️
+});
+
+export const validateFormInput = (values: unknown) => {
+  const parsedData = Form.parse(values);
+
+  return parsedData;
+};
+
+// TESTS
+
+it('Should validate correct inputs', async () => {
+  expect(() =>
+    validateFormInput({
+      name: 'Matt',
+    })
+  ).not.toThrow();
+
+  expect(() =>
+    validateFormInput({
+      name: 'Matt',
+      phoneNumber: '123',
+    })
+  ).not.toThrow();
+});
+
+it('Should throw when you do not include the name', async () => {
+  expect(() => validateFormInput({})).toThrowError('name is required');
+});
