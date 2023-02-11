8d3d6e767adf4fbdc58fcc151a7585a2c9b4f157
victorlongon <victor.longon@hiberworld.com>
2023-02-11 21:18:56 +0100

    07 union problem

---
+++ 07-unionProblem.test.ts
@@ -0,0 +1,43 @@
+import { expect, it } from 'vitest';
+import { z } from 'zod';
+
+const Form = z.object({
+  repoName: z.string(),
+  privacyLevel: z.enum(['private', 'public']),
+  //or
+  // privacyLevel: z.union([z.literal('private'), z.literal('public')]),
+  // z.enum is a shorthand for the above
+});
+
+export const validateFormInput = (values: unknown) => {
+  const parsedData = Form.parse(values);
+
+  return parsedData;
+};
+
+// TESTS
+
+it('Should fail if an invalid privacyLevel passed', async () => {
+  expect(() =>
+    validateFormInput({
+      repoName: 'mattpocock',
+      privacyLevel: 'something-not-allowed',
+    })
+  ).toThrowError();
+});
+
+it('Should permit valid privacy levels', async () => {
+  expect(
+    validateFormInput({
+      repoName: 'mattpocock',
+      privacyLevel: 'private',
+    }).privacyLevel
+  ).toEqual('private');
+
+  expect(
+    validateFormInput({
+      repoName: 'mattpocock',
+      privacyLevel: 'public',
+    }).privacyLevel
+  ).toEqual('public');
+});
